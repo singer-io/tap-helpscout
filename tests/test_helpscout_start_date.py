170195c8c05e492d5196fdf126873ca59a107563
Manoj Kumar Anand <manand@talend.com>
2021-11-15 19:30:26 +0000

    added assertions to valdiate the replication key is greater than start date for both the syncs

--- tests/test_helpscout_start_date.py
+++ tests/test_helpscout_start_date.py
@@ -25,7 +25,6 @@ class StartDateTest(HelpscoutBaseTest):
 
 
         self.start_date_2 = '2021-11-01T00:00:00Z'
-        self.start_date_2 = datetime.datetime.strftime(self.start_date_2,"%Y-%m-%dT00:00:00Z")
         self.start_date_2 = self.timedelta_formatted(self.start_date_2, days = -2)
 
         self.start_date = self.start_date_1
@@ -42,7 +41,7 @@ class StartDateTest(HelpscoutBaseTest):
         # run check mode
         found_catalogs_1 = self.run_and_verify_check_mode(conn_id_1)
 
-         # table and field selection
+        # table and field selection
         test_catalogs_1_all_fields = [catalog for catalog in found_catalogs_1
                                       if catalog.get('tap_stream_id') in streams_to_test]
         self.perform_and_verify_table_and_field_selection(conn_id_1, test_catalogs_1_all_fields, select_all_fields=True)
@@ -82,8 +81,9 @@ class StartDateTest(HelpscoutBaseTest):
 
                 # expected values
                 expected_primary_keys = self.expected_primary_keys()[stream]
-                expected_start_date_1 = self.start_date_1
+                expected_start_date_1 = self.start_date_1['start_date']
                 expected_start_date_2 = self.start_date_2
+                expected_replication_keys = self.expected_replication_keys()[stream]
 
                 # All the streams obey the start date
                 # collect information for assertions from sync 1 and sync 2 based on expected values
@@ -99,9 +99,28 @@ class StartDateTest(HelpscoutBaseTest):
                 primary_keys_sync_2 = set(primary_keys_list_2)
 
 
+                replication_key_sync_1 = [message.get('data').get(expected_rk) for expected_rk in expected_replication_keys
+                                          for message in synced_records_1.get(stream).get('messages')
+                                          if message.get('action') == 'upsert']
+                replication_key_sync_2 = [message.get('data').get(expected_rk) for expected_rk in expected_replication_keys
+                                          for message in synced_records_2.get(stream, {'messages': []}).get('messages')
+                                          if message.get('action') == 'upsert']
+
+                replication_key_sync_1 = list(replication_key_sync_1)
+                replication_key_sync_2 = list(replication_key_sync_2)
+
+
                 # Verify the number of records replicated in sync 1 is greater than the number
                 # of records replicated in sync 2
                 self.assertGreaterEqual(record_count_sync_1, record_count_sync_2)
 
                 # Verify the records replicated in sync 2 were also replicated in sync 1
                 self.assertTrue(primary_keys_sync_2.issubset(primary_keys_sync_1))
+
+                # Verify that the replication keys in sync 1 are greater than or equal to start_date_1
+                for rk in replication_key_sync_1:
+                    self.assertGreaterEqual(rk, expected_start_date_1)
+
+                # Verify that the replication keys in sync 2 are greater than or equal to start_date_2
+                for rk in replication_key_sync_2:
+                    self.assertGreaterEqual(rk, expected_start_date_2)
