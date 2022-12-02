881b04da25ebe4d03acaf9c811dfda14ba676d1e
kethan1122 <kcherukuri@talend.com>
2022-12-02 16:07:04 +0530

    upgrade singer-python

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-helpscout',
       install_requires=[
           'backoff==1.8.0',
           'requests==2.23.0',
-          'singer-python==5.9.0'
+          'singer-python==5.13.0'
       ],
       entry_points='''
           [console_scripts]
