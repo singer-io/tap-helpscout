47cd7157ac8a663942d461feee396e7570641795
kethan1122 <kcherukuri@talend.com>
2023-01-24 15:50:55 +0530

    key_properties to list

--- tap_helpscout/streams/mailbox_folders.py
+++ tap_helpscout/streams/mailbox_folders.py
@@ -7,7 +7,7 @@ class MailBoxFolders(IncrementalStream, ABC):
     """Class for `mailbox_folders` stream"""
     stream = tap_stream_id = "mailbox_folders"
     path = "/mailboxes/{}/folders"
-    key_properties = "id"
+    key_properties = ["id"]
     replication_key = "updated_at"
     replication_key_type = "datetime"
     valid_replication_keys = ("updated_at",)
