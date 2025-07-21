164f3a5974f1600b4fbf80b026a0629a103cbdd0
Andy Lu <luandy64@gmail.com>
2025-07-21 14:05:14 -0400

    Bump versions (#40)
    * Bump versions
    
    * Bump versions

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="tap-helpscout",
-    version="1.2.0",
+    version="1.2.1",
     description="Singer.io tap for extracting data from the HelpScout Mailbox API 2.0",
     author="jeff.huth@bytecode.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_helpscout"],
-    install_requires=["backoff==1.8.0", "requests==2.23.0", "singer-python==5.13.0"],
+    install_requires=["backoff==1.8.0", "requests==2.32.4", "singer-python==5.13.2"],
     extras_require={
         "dev": [
             "ipdb",
