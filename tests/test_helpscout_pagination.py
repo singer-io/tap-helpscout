f27ef05a3d52a0e692160684d642b1621f961f81
Ben Allred <ben.allred@qlik.com>
2025-11-11 15:21:56 +0000

    pagination

--- tests/test_helpscout_pagination.py
+++ tests/test_helpscout_pagination.py
@@ -45,7 +45,7 @@ class PaginationTest(HelpscoutBaseTest):
                 # streams from this assertion
                 # Spike created to address this issue : TDL - 16378
 
-                if stream in ('conversations',):
+                if stream in ('conversations', 'conversation_threads'):
                     stream_page_size = self.expected_page_limits()[stream]
                     self.assertLessEqual(stream_page_size, record_count)
 
