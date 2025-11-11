f86ee6ee5acdd7adc429eacfc43b5688d28dc01e
Ben Allred <ben.allred@qlik.com>
2025-11-11 14:55:14 +0000

    Fix pagination test

--- tests/test_helpscout_pagination.py
+++ tests/test_helpscout_pagination.py
@@ -45,9 +45,9 @@ class PaginationTest(HelpscoutBaseTest):
                 # streams from this assertion
                 # Spike created to address this issue : TDL - 16378
 
-                if stream not in ('users','workflows','mailboxes','mailbox_fields','mailbox_folders','happiness_ratings_report', 'teams', 'team_members'):
+                if stream in ('conversations',):
                     stream_page_size = self.expected_page_limits()[stream]
-                    self.assertLessEqual(record_count, stream_page_size)
+                    self.assertLessEqual(stream_page_size, record_count)
 
                 # Verify there are no duplicates across pages
                 records_pks_set = {
